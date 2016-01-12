b08a731dd0da83349d22a2ce2d4b3795508437ea
JacobLandau <Jacob@PopcornFlicks.ca>
2016-01-12 16:46:59 -0500

    Syntax error

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='pykcd',
     version = '1.0.0',
-    description = 'Python interface/wrapper for the XKCD API'
+    description = 'Python interface/wrapper for the XKCD API',
     url = 'https://github.com/JacobLandau/pykcd',
     author = 'Jacob Landau',
     author_email = 'Jacob@PopcornFlicks.ca',
